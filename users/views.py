958ef746abd28251f2943a9f061ff97e2893cba5
kosazna <kaznavouridis@gmail.com>
2021-02-24 22:35:02 +0200

    (L)

--- users/views.py
+++ users/views.py
@@ -1,7 +1,17 @@
-from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import UserRegisterForm
 
 
 def register(response):
-    form = UserCreationForm()
+    if response.method == 'POST':
+        form = UserRegisterForm(response.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(response, f"Account created for {username}")
+            return redirect('blog-home')
+    else:
+        form = UserRegisterForm()
+
     return render(response, 'users/register.html', {'form': form})
