1755b721e63c5050898dda7ac6c60af45ca32508
kosazna <kaznavouridis@gmail.com>
2021-02-24 23:20:55 +0200

    (L)

--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from .forms import UserRegisterForm
 
 
@@ -9,9 +10,15 @@ def register(response):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(response, f"Account created for {username}")
-            return redirect('blog-home')
+            messages.success(
+                response, f"Your account has been created. You are able to login!")
+            return redirect('login')
     else:
         form = UserRegisterForm()
 
     return render(response, 'users/register.html', {'form': form})
+
+
+@login_required
+def profile(response):
+    return render(response, 'users/profile.html')
